b51b24e79ca6c4a257fa7d384dde5cf969dc9046
martina.dre <martina.dre@centrum.cz>
2022-04-16 14:31:38 +0200

    Úkol01 - Část 1

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,63 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+
+        nakresliPrasatko(zofka);
+
+
+    }
+
+    private void nakresliPrasatko(Turtle zofka) {
+        // tělo
+        for (int i = 0; i < 2; i++) {
+            zofka.turnRight(90);
+            zofka.move(120);
+            zofka.turnRight(90);
+            zofka.move(90);
+        }
+        for (int i = 0; i < 2; i++) {
+            zofka.turnLeft(120);
+            zofka.move(90);
+        }
+            zofka.turnRight(90);
+        // nohy
+        nakresliNohu(zofka);
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(120);
+        zofka.turnRight(120);
+        zofka.penDown();
+        nakresliNohu(zofka);
+        // ocasek
+        zofka.penUp();
+        zofka.move(90);
+        zofka.turnRight(150);
+        zofka.penDown();
+        nakresliVlnkuOcasku(zofka);
+        for (int j = 0; j < 15; j++) {
+            zofka.move(2);
+            zofka.turnLeft(12);
+        }
+        nakresliVlnkuOcasku(zofka);
+        zofka.turnRight(30);
+    }
+
+    private void nakresliVlnkuOcasku(Turtle zofka) {
+        for (int i = 0; i < 20; i++) {
+            zofka.move(3);
+            zofka.turnLeft(9);
+        }
+    }
+
+    private void nakresliNohu(Turtle zofka) {
+        zofka.move(40);
+        zofka.turnLeft(120);
+        zofka.penUp();
+        zofka.move(40);
+        zofka.turnLeft(120);
+        zofka.penDown();
+        zofka.move(40);
+        zofka.turnRight(30);
     }
 
 }
